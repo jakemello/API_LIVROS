7369059dbf33185354f543618c094ebaf08c92bb
jakemello <jakelinemello@live.com>
2025-03-15 12:57:15 -0300

    Criando API

--- app.py
+++ app.py
@@ -1,55 +1,48 @@
-from flask import Flask  # Aqui estamos importando a classe Flask do módulo flask para criar nossa aplicação
-import sqlite3  # Importamos o módulo sqlite3 para manipulação do banco de dados SQLite
+from flask import Flask, request, jsonify
 
-# Criamos uma instância do Flask e armazenamos na variável "app"
-# O parâmetro __name__ indica que este arquivo será reconhecido como o principal da aplicação
+import sqlite3
+
 app = Flask(__name__)
 
-# Aqui estamos criando uma rota para o endpoint "/"
-# Ou seja, quando acessarmos http://127.0.0.1:5000/, a função abaixo será executada
-@app.route("/")
+# --> São os endpoint da nossa API
+@app.route("/pague")
 def pagar_pessoas():
-    # Retorna uma mensagem formatada em HTML para ser exibida na página principal
-    return "<h1>Começar a semana, pagando suas dívidas, é bom demais</h1>"
 
-# Criamos uma rota para o endpoint "/pix"
-# Quando acessarmos http://127.0.0.1:5000/pix, a função será chamada automaticamente
-@app.route("/pix")
-def mande_o_pix():
-    # Retorna uma mensagem formatada em HTML para ser exibida na rota "/pix"
-    return "<h3>Pagar as pessoas faz bem pras pessoas!!! =D</h3>"
+    return "<h1>começar a semana pagando suas dívidas, é bom demais</h1>"
 
-# Criamos uma rota para o endpoint "/comidas"
-# Quando acessarmos http://127.0.0.1:5000/comidas, essa função será executada
-@app.route("/comidas")
-def prato_do_dia():
-    # Retorna uma mensagem formatada em HTML descrevendo o prato do dia
-    return "<h1>O prato do dia é feijoada com farofinha com bacon e de sobremesa brownie com sorvete!!!</h1>"
-
-# Função para inicializar o banco de dados SQLite
-# Criamos uma conexão com o banco de dados chamado 'database.db'
-# Se o banco de dados ainda não existir, ele será criado automaticamente
 
 def init_db():
-    # Conectamos ao banco de dados SQLite e usamos "with" para garantir que a conexão seja fechada corretamente após a execução
+    #Crie o nosso banco de dados com um arquivo 'databse.db' e conecte a variável conn(connection)
     with sqlite3.connect("database.db") as conn:
-        # Executamos um comando SQL para criar a tabela LIVROS, caso ela ainda não exista
         conn.execute(
-            """
+           """"
                 CREATE TABLE IF NOT EXISTS LIVROS(
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,  # Identificador único para cada livro, gerado automaticamente
-                    titulo TEXT NOT NULL,  # O título do livro, armazenado como texto e obrigatório
-                    categoria TEXT NOT NULL,  # A categoria do livro (exemplo: ficção, tecnologia), armazenada como texto e obrigatória
-                    autor TEXT NOT NULL,  # O nome do autor do livro, armazenado como texto e obrigatório
-                    imagem_url TEXT NOT NULL  # O URL da imagem da capa do livro, armazenado como texto e obrigatório
-                )
-            """
-        )  # A execução desse comando cria a tabela caso ela ainda não exista, garantindo que nossa estrutura de banco esteja configurada
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    titulo TEXT NOT NULL,
+                    categoria TEXT NOT NULL,
+                    autor TEXT NOT NULL,
+                    imagem_url TEXT NOT NULL
+               )
+           """ 
+        )
 
-# Chamamos a função para inicializar o banco de dados quando o programa for executado
-init_db()
+    init_db()  
 
-# Aqui verificamos se o script está sendo executado diretamente e não importado como módulo
+    @app.route("/doar", methods=["POST"])
+    def doar():
+
+        dados = request.get_json()
+
+        titulo = dados.get("titulo")
+        categoria = dados.get("categoria")
+        autor = dados.get("autor")
+        imagem_url = dados.get("imagem_url")
+
+        if not titulo or not categoria or not autor or not imagem_url:
+            return jsonify({"erro":"Todos os campos são obrigatórios"}),400
+
+# --> É o comando para rodar a nossa aplicação
+# --> Se o arquivo app.py for igual(==) ao arquivo principal da nossa aplicação
 if __name__ == "__main__":
-    # Inicia o servidor Flask no modo de depuração (nesse modo nossa API responde automaticamente a qualquer atualização que fizermos no código)
     app.run(debug=True)
+
