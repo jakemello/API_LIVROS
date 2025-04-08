4b6d7216f51788f509acc01c3dde873ec2fb394d
jakemello <jakelinemello@live.com>
2025-04-08 14:27:12 -0300

    Adicionei uma estrutura para deixar os livros salvos no banco de dados

--- app.py
+++ app.py
@@ -14,19 +14,48 @@ def mensagem():
 
 
 def init_db():
-    # Crie o nosso banco de dados com um arquivo 'databse.db' e conecte a variável conn(connection)
     with sqlite3.connect("database.db") as conn:
-        conn.execute(
-            """
-                CREATE TABLE IF NOT EXISTS LIVROS(
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    titulo TEXT NOT NULL,
-                    categoria TEXT NOT NULL,
-                    autor TEXT NOT NULL,
-                    image_url TEXT NOT NULL
-               )
-           """
-        )
+        conn.execute("""
+            CREATE TABLE IF NOT EXISTS LIVROS(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                titulo TEXT NOT NULL,
+                categoria TEXT NOT NULL,
+                autor TEXT NOT NULL,
+                image_url TEXT NOT NULL
+            )
+        """)
+        #Estrutura adiciona para deixar os livros salvos no banco de dados
+        # e não precisar ficar adicionando sempre que reiniciar o servidor da aplicação.
+        quantidade = conn.execute("SELECT COUNT(*) FROM livros").fetchone()[0]
+
+        if quantidade == 0:
+            livros_padrao = [
+
+                ("A Arte da Dívida", "Finanças", "Fernanda TZU", "https://i.imgur.com/Zj1iQoD.jpeg"),
+
+                ("O Pequeno Príncipe", "Ficção", "Antoine de Saint-Exupéry","https://m.media-amazon.com/images/I/61ATa0Pc4AL._AC_UF1000,1000_QL80_.jpg"),
+
+                 ("A Menina que Roubava Livros", "Ficção", "Markus Zusak", "https://m.media-amazon.com/images/I/61L+4OBhm-L._AC_UF1000,1000_QL80_.jpg"),
+
+                 ("Game of Thrones", "Fantasia", "George R. R. Martin", "https://m.media-amazon.com/images/I/91+1SUO3vUL.jpg"),
+
+                 ("O Retrato de Dorian Gray", "Clássico", "Oscar Wilde", "https://m.media-amazon.com/images/I/91ZEEdj0cCL._AC_UF1000,1000_QL80_.jpg"),
+
+                 ("Harry Potter e as Relíquias da Morte", "Fantasia", "J.K. Rowling", "https://m.media-amazon.com/images/I/81rvO7xcJOL._AC_UF1000,1000_QL80_.jpg"),
+
+                 ("Diario de um Banana", "Infantil", "Jeff Kinney", "https://m.media-amazon.com/images/I/71fWaI5myqL._AC_UF1000,1000_QL80_.jpg"),
+
+                 ("League of Legends: Realms of Runeterra", "Universo de Ficção", "Evan Narcisse", "https://m.media-amazon.com/images/I/81gH-PXavoL._UF894,1000_QL80_.jpg"),
+            ]
+
+            for livro in livros_padrao:
+                titulo, categoria, autor, image_url = livro
+                conn.execute("""
+                    INSERT INTO LIVROS (titulo, categoria, autor, image_url)
+                    VALUES (?, ?, ?, ?)
+                """, (titulo, categoria, autor, image_url))
+
+            conn.commit()
 
 
 init_db()
@@ -51,8 +80,8 @@ def doar():
 
         conn.execute(f"""
             INSERT INTO Livros (titulo, categoria, autor, image_url)
-            VALUES ("{titulo}", "{categoria}", "{autor}", "{image_url}")
-            """)
+            VALUES (?, ?, ?, ?)
+            """, (titulo, categoria, autor, image_url))
 
     conn.commit()
 
